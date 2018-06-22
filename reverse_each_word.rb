fd768b80df1abf11638a2388cb7bbbe436ed3329
Sid He <xhe518@gmail.com>
2018-06-22 23:59:30 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  splitted_sentence = sentence.split(" ")
+  def sentence_operation(splitted_sentence)
+    splitted_sentence.collect do |word|
+      word.reverse
+    end
+  end
+  sentence_operation(splitted_sentence).join(" ")
+end
