9f9887a954f7388c4dadb16859be4e5be0950fe5
Sid He <xhe518@gmail.com>
2018-06-22 23:58:23 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  splitted_sentence = sentence.split(" ")
+  def sentence_operation(splitted_sentence)
+    splitted_sentence.map do |word|
+      word.reverse
+    end
+  end
+  sentence_operation.join(" ")
+end
